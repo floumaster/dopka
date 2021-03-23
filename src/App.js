fc89ff8b5c8fe67a67894a69f78d9564a7f8dc02
floumaster <dimaslubitsk@gmail.com>
2021-03-23 22:26:23 +0200

    f

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import './App.css';
 import React, { useEffect } from 'react';
-
-function selectQuestion(e){
-  e.currentTarget.classList.toggle("selected")
-}
+import Questions from './questions/Questions'
+import Images from './images/Images'
 
 function App() {
 
@@ -14,77 +12,34 @@ function App() {
     return () => clearInterval(interval);
   }, []);
 
+  const faq = [
+    {
+      question: 'How many team members can I invite?',
+      answer: 'You can invite up to three members. To get bonuses, share your referral link with your friends',
+    },
+    {
+      question: 'What is the maximum file upload size?',
+      answer: 'No more than 2GB. All files in your account must fit your allotted storage space.',
+    },
+    {
+      question: 'How do I reset my password?',
+      answer: 'You can invite up to three members. To get bonuses, share your referral link with your friends',
+    },
+    {
+      question: 'Can I cancel my subscription?',
+      answer: 'You can invite up to three members. To get bonuses, share your referral link with your friends',
+    },
+    {
+      question: 'Do you provide additional support?',
+      answer: 'You can invite up to three members. To get bonuses, share your referral link with your friends',
+    },
+  ]
+
   return (
     <div className="App">
         <section className="questions-wrapper">
-            <div className="images">
-              <div className="pattern-wrapper">
-                <img src="bg-pattern-desktop.svg" alt="pattern"/>
-              </div>
-              <div className="woman-wrapper">
-                <img src="illustration-woman-online-desktop.svg" alt="woman"/>
-              </div>
-            </div>
-            <div className="questions">
-              <h1 className="logo">FAQ</h1>
-              <ul className="question-list">
-            <li className="question">
-              <section className="question_short" onClick={selectQuestion}>
-                <div className="text">How many team members can I invite?</div>
-                <div className="img">
-                  <img className="arrow" src="icon-arrow-down.svg" alt="icon-arrow-down"/>
-                </div>
-              </section>
-              <section className="question_answer">
-                You can invite up to three members. To get bonuses, share your referral link with your friends
-              </section>
-            </li>
-            <li className="question">
-              <section className="question_short" onClick={selectQuestion}>
-                <div className="text">What is the maximum file upload size?</div>
-                <div className="img">
-                  <img className="arrow" src="icon-arrow-down.svg" alt="icon-arrow-down"/>
-                </div>
-              </section>
-              <section className="question_answer">
-                No more than 2GB. All files in your account must fit your allotted storage space.
-              </section>
-            </li>
-            <li className="question">
-              <section className="question_short" onClick={selectQuestion}>
-                <div className="text">How do I reset my password?</div>
-                <div className="img">
-                  <img className="arrow" src="icon-arrow-down.svg" alt="icon-arrow-down"/>
-                </div>
-              </section>
-              <section className="question_answer">
-                You can invite up to three members. To get bonuses, share your referral link with your friends
-              </section>
-            </li>
-            <li className="question">
-              <section className="question_short" onClick={selectQuestion}>
-                <div className="text">Can I cancel my subscription?</div>
-                <div className="img">
-                  <img className="arrow" src="icon-arrow-down.svg" alt="icon-arrow-down"/>
-                </div>
-              </section>
-              <section className="question_answer">
-                You can invite up to three members. To get bonuses, share your referral link with your friends
-              </section>
-            </li>
-            <li className="question">
-              <section className="question_short" onClick={selectQuestion}>
-                <div className="text">Do you provide additional support?</div>
-                <div className="img">
-                  <img className="arrow" src="icon-arrow-down.svg" alt="icon-arrow-down"/>
-                </div>
-              </section>
-              <section className="question_answer">
-                You can invite up to three members. To get bonuses, share your referral link with your friends
-              </section>
-            </li>
-          </ul>
-            </div>
+            <Images/>
+            <Questions faq={faq}/>
         </section>
     </div>
   );
